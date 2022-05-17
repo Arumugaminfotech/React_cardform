dac33e5ef6963f3540d3a535f34a51d589af759e
arumugam.s <96759953+Arumugaminfotech@users.noreply.github.com>
2022-05-17 17:58:14 +0530

    Update card.js

--- src/card.js
+++ src/card.js
@@ -1,18 +1,15 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import moment from 'moment'
 import Button from '@mui/material/Button';
@@ -20,18 +17,6 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { bgcolor } from '@mui/system';
 import {Grid} from '@mui/material'
-
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
 export default function RecipeReviewCard(props) {
   const [expanded, setExpanded] = React.useState(false);
@@ -55,7 +40,6 @@ export default function RecipeReviewCard(props) {
 
   return (
     <div  className="card">
-      <Grid>
     <Card sx={{ maxWidth: 345,  background:" linear-gradient(135deg, hsl(36,100%,50%) 10%, hsl(72,100%,50%) 60%, white 90%)",}} marginLeft="100px" >
       <CardHeader 
         avatar={
@@ -94,10 +78,13 @@ export default function RecipeReviewCard(props) {
         <MenuItem onClick={props.remove}>Delete</MenuItem>
         <MenuItem onClick={props.h}>Edit</MenuItem>
       </Menu>
+      <img src="Tringer.jpg"></img>
+
       <CardMedia
         component="img"
         height="194"
-        image="./image/mumbai.jpg"
+        src = "./image/mumbai.jpg"
+        
         alt="Paella dish"
       />
       <CardContent>
@@ -115,23 +102,9 @@ export default function RecipeReviewCard(props) {
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>
-          Details of the project will be updated soon
-          </Typography>
-        </CardContent>
-      </Collapse>
+       
+      </CardActions>     
     </Card>
-    </Grid>
     </div>
   );
 }
